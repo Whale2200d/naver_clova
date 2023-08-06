1c0673b03a031aacf1742dd27a124eed48be81cf
임경락 <skatewang99@gmail.com>
2023-08-06 17:00:11 +0900

    Refactor : recognition.lang을 'ko-KR'로 변경

--- src/pages/SpeechToText/index.tsx
+++ src/pages/SpeechToText/index.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const SpeechToText: React.FC = () => {
+  const navigate = useNavigate();
+
   const [isListening, setIsListening] = useState(false);
   const [transcript, setTranscript] = useState('');
 
@@ -9,17 +12,26 @@ const SpeechToText: React.FC = () => {
       const SpeechRecognition =
         window.SpeechRecognition || window.webkitSpeechRecognition;
       const recognition = new SpeechRecognition();
+      if (!recognition) {
+        alert(
+          'speech API를 사용할 수 없는 브라우저입니다. 브라우저를 확인해주세요.'
+        );
+        navigate('/speechtotext');
+      }
       console.log('recognition: ', recognition);
-      recognition.lang = 'en-US';
+      recognition.lang = 'ko-KR';
+      // recognition.lang = 'en-US';
 
       recognition.onstart = () => {
         console.log('Listening...');
       };
 
       recognition.onresult = (event) => {
+        console.log('event.results: ', event.results);
         const lastResultIndex = event.results.length - 1;
+        console.log('lastResultIndex:', lastResultIndex);
         const spokenText = event.results[lastResultIndex][0].transcript;
-        console.log(spokenText);
+        console.log('spokenText: ', spokenText);
         setTranscript(spokenText);
       };
 
