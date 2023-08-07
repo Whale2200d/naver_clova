015c753c367b87107493180cfc87ccf00b693466
임경락 <skatewang99@gmail.com>
2023-08-07 17:35:49 +0900

    Feat : 이메일 및 비밀번호 유효성 검사 기능 추가

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,23 +1,55 @@
 import { useNavigate } from 'react-router-dom';
+import * as S from './styles';
+import { useState } from 'react';
 
 function SignIn() {
   const navigate = useNavigate();
 
+  const [isEmailValid, setIsEmailValid] = useState(true);
+  const [isPasswordValid, setIsPasswordValid] = useState(true);
+
   const handleClickSignIn = () => {
-    navigate('/SpeechToText');
+    navigate('/speechtotext');
   };
 
   const handleClickSignUp = () => {
-    navigate('/SignUp');
+    navigate('/signup');
   };
 
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const email = e.target.value;
+    const isEmailValid = email.includes('@');
+    setIsEmailValid(isEmailValid);
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const password = e.target.value;
+    const isPasswordValid = password.length >= 8;
+    setIsPasswordValid(isPasswordValid);
+  };
   return (
-    <div>
-      <input type="email" />
-      <input type="password" />
-      <button onClick={handleClickSignIn}>로그인</button>
-      <button onClick={handleClickSignUp}>회원가입</button>
-    </div>
+    <S.Container>
+      <S.Input
+        type="email"
+        // value={email}
+        onChange={handleEmailChange}
+        placeholder="이메일을 입력하세요."
+      />
+      {!isEmailValid && (
+        <S.Span>이메일 형식이 틀렸습니다. @를 넣어주세요.</S.Span>
+      )}
+      <S.Input
+        type="password"
+        // value={password}
+        onChange={handlePasswordChange}
+        placeholder="비밀번호를 입력하세요."
+      />
+      {!isPasswordValid && (
+        <S.Span>비밀번호 형식이 틀렸습니다. 8자리 이상 입력해주세요.</S.Span>
+      )}
+      <S.Button onClick={handleClickSignIn}>로그인</S.Button>
+      <S.Button onClick={handleClickSignUp}>회원가입</S.Button>
+    </S.Container>
   );
 }
 
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,19 +1,50 @@
-import React from 'react';
+import { useState } from 'react';
+import * as S from './styles';
 import { useNavigate } from 'react-router-dom';
 
 function SignUp() {
   const navigate = useNavigate();
 
+  const [isEmailValid, setIsEmailValid] = useState(true);
+  const [isPasswordValid, setIsPasswordValid] = useState(true);
+
   const handleClickSignUp = () => {
     navigate('/signin');
   };
 
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const email = e.target.value;
+    const isEmailValid = email.includes('@');
+    setIsEmailValid(isEmailValid);
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const password = e.target.value;
+    const isPasswordValid = password.length >= 8;
+    setIsPasswordValid(isPasswordValid);
+  };
   return (
-    <>
-      <input type="email" />
-      <input type="password" />
-      <button onClick={handleClickSignUp}>회원가입</button>
-    </>
+    <S.Container>
+      <S.Input
+        type="email"
+        // value={email}
+        onChange={handleEmailChange}
+        placeholder="이메일을 입력하세요."
+      />
+      {!isEmailValid && (
+        <S.Span>이메일 형식이 틀렸습니다. @를 넣어주세요.</S.Span>
+      )}
+      <S.Input
+        type="password"
+        // value={password}
+        onChange={handlePasswordChange}
+        placeholder="비밀번호를 입력하세요."
+      />
+      {!isPasswordValid && (
+        <S.Span>비밀번호 형식이 틀렸습니다. 8자리 이상 입력해주세요.</S.Span>
+      )}
+      <S.Button onClick={handleClickSignUp}>가입하기</S.Button>
+    </S.Container>
   );
 }
 
